1f032c2b7675ad6d71c3af306267c0ef3ea0796a
Sawom <asawom250@gmail.com>
2023-08-12 00:56:59 +0600

    parallax added

--- src/Pages/Homepage/BannerThree/BannerThree.js
+++ src/Pages/Homepage/BannerThree/BannerThree.js
@@ -4,20 +4,20 @@ import bannerImg from '../../../assets/home/featured.jpg';
 
 const BannerThree = () => {
     return (
-        <div className='mt-10 backImage  bg-fixed text-white pt-8 my-20'>
+        <div className='mt-10 mx-auto backImage  bg-fixed text-white pt-8 my-20'>
             {/* heading  */}
             <section>
 
             </section>
             {/* others */}
-            <div className="md:flex container mx-auto justify-center items-center bg-slate-500 bg-opacity-60 pb-20 pt-12 px-36">
-                <div>
-                    <img className='w-50' src={bannerImg} alt="" />
+            <div className="md:flex  lg:flex space-x-4  bg-slate-700 bg-opacity-70 pb-20 pt-12 px-36">
+                <div className='w-1/2' >
+                    <img  src={bannerImg} alt="" />
                 </div>
-                <div className="md:ml-10">
-                     <p>Aug 20, 2029</p>
-                    <p className="uppercase">Where can i get some?</p>
-                    <button className="btn btn-outline border-0 border-b-4 mt-4">Order Now</button>
+                <div className="md:ml-10 w-1/2">
+                    <p className="uppercase text-2xl font-bold">Where can i get some?</p>
+                    <p>Indulge your taste buds at ARS Restaurant  Join us for a culinary adventure that will satisfy your cravings. Our talented chefs have crafted a diverse menu, featuring mouthwatering appetizers, hearty main courses, and delectable desserts. Whether you're in the mood for a juicy burger, a classic pizza, or a refreshing salad, we have something for everyone.</p>
+                    <button className="btn btn-outline text-white border-4 mt-6">Order Now</button>
                 </div>
             </div>
         </div>
--- src/Pages/Homepage/Bannertwo/Bannertwo.js
+++ src/Pages/Homepage/Bannertwo/Bannertwo.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
-import img6 from '../../../assets/home/service.jpg';
+import './Bannertwo.css'
 
 const Bannertwo = () => {
     return (
-        <div  className='container mx-auto mt-20' >
-            <ParallaxProvider>
-                <ParallaxBanner
-                    layers={[
-                        { image: img6, speed: -20 },
-                        {
-                        speed: -10,
-                        children: (
-                            <div className="absolute inset-0 flex items-center justify-center">
-                            <h1 className="text-5xl text-yellow-400 font-bold">We Provide Best Food For You!</h1>
-                            </div>
-                        ),
-                        },
-                    ]}
-                    className="aspect-[2/1]"
-                    />
-             </ParallaxProvider>
+        <div  className=' mt-10 mx-auto container backgroundImage bg-fixed text-white pt-8 my-20' >
+            {/* parallax effect. create manually */}
+             <div className="  bg-slate-600 bg-opacity-70 pb-20 pt-22 p-36">
+                <p className='text-3xl text-center font-bold ' >We Provide Best Food For You!</p>
+                
+            </div>
             
         </div>
     );
