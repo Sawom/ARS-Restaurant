30a2246914ef1ba437ca60b64e9cc89b06d538ec
Sawom <asawom250@gmail.com>
2023-08-11 17:04:54 +0600

    banner2 added

---
+++ src/Pages/Homepage/Bannertwo/Bannertwo.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ParallaxBanner, ParallaxProvider } from 'react-scroll-parallax';
+import img6 from '../../../assets/home/service.jpg';
+
+const Bannertwo = () => {
+    return (
+        <div  className='container mx-auto mt-20' >
+            <ParallaxProvider>
+                <ParallaxBanner
+                    layers={[
+                        { image: img6, speed: -20 },
+                        {
+                        speed: -10,
+                        children: (
+                            <div className="absolute inset-0 flex items-center justify-center">
+                            <h1 className="text-5xl text-yellow-400 font-bold">We Provide Best Food For You!</h1>
+                            </div>
+                        ),
+                        },
+                        
+                    ]}
+                    className="aspect-[2/1]"
+                    />
+             </ParallaxProvider>
+            
+        </div>
+    );
+};
+
+export default Bannertwo;
--- src/Pages/Homepage/Home/Home.js
+++ src/Pages/Homepage/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import FoodCategory from '../FoodCategory/FoodCategory';
+import Bannertwo from '../Bannertwo/Bannertwo';
 
 const Home = () => {
     return (
         <div className=''>
             <Banner></Banner>
             <FoodCategory></FoodCategory>
+            <Bannertwo></Bannertwo>
         </div>
     );
 };
