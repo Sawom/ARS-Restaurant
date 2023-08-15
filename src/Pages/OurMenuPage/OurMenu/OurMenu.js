89f66c56145d7d842373d36feb17c7a0a9f67758
Sawom <asawom250@gmail.com>
2023-08-15 23:48:30 +0600

    top banner added

--- src/Pages/OurMenuPage/OurMenu/OurMenu.js
+++ src/Pages/OurMenuPage/OurMenu/OurMenu.js
@@ -13,22 +13,25 @@ const OurMenu = () => {
     const drinks = menu.filter( (item)=> item.category === 'drinks' );
 
     return (
-        <div className='mx-auto container' >
+        <div className='' >
             {/* ourmenu page er page er shb kichu ei jaygay load kortechi */}
             <TopBanner></TopBanner>
-            {/*  */}
-            <p>todays offer</p>
-            <MenuCategory foodItem={offered} ></MenuCategory>
-            <p>dessert</p>
-            <MenuCategory foodItem={dessert} ></MenuCategory>
-            pizza
-            <MenuCategory foodItem={pizza} ></MenuCategory>
-             salad
-            <MenuCategory foodItem={salad} ></MenuCategory>
-            soup
-            <MenuCategory foodItem={soup} ></MenuCategory>
-            drinks
-            <MenuCategory foodItem={drinks} ></MenuCategory>
+            <div className='container mx-auto'>
+                {/* load and filter menu */}
+                <p>todays offer</p>
+                <MenuCategory foodItem={offered} ></MenuCategory>
+                <p>dessert</p>
+                <MenuCategory foodItem={dessert} ></MenuCategory>
+                pizza
+                <MenuCategory foodItem={pizza} ></MenuCategory>
+                salad
+                <MenuCategory foodItem={salad} ></MenuCategory>
+                soup
+                <MenuCategory foodItem={soup} ></MenuCategory>
+                drinks
+                <MenuCategory foodItem={drinks} ></MenuCategory>
+            </div>
+            
         </div>
     );
 };
--- src/Pages/OurMenuPage/TopBanner/TopBanner.js
+++ src/Pages/OurMenuPage/TopBanner/TopBanner.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './TopBanner.css';
 
 const TopBanner = () => {
     return (
         <div>
-            
+            <div className="hero lg:h-[550px] topBanner">
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md mb-8">
+                    <p className="mb-5 text-4xl font-bold">OUR MENU</p>
+                    <p className="mb-5 text-xl font-bold"> Would You Like to Try a Dish? </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
