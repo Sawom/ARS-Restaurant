6b027065cf0e07c070fe972dda18161765d17350
Sawom <asawom250@gmail.com>
2023-08-30 12:50:02 +0600

    login register function added

--- src/Pages/Authentication/useFirebase/useFirebase.js
+++ src/Pages/Authentication/useFirebase/useFirebase.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut}  from "firebase/auth";
+import {GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile}  from "firebase/auth";
 import initializeFirebase from '../Firebase/firebase.init';
 import { useState } from 'react';
 import { useEffect } from 'react';
@@ -10,6 +10,7 @@ const useFirebase = () => {
     const [user,setUser] = useState({});
     const [authError, setAuthError] = useState('');
 
+    // const auth = getAuth(); eta google sign in , email register , login shb jaygay lage jonno ekbarei nichilam
     const auth = getAuth();
 
     // google login
@@ -29,7 +30,47 @@ const useFirebase = () => {
                 const email = error.customData.email;
                 const credential = GoogleAuthProvider.credentialFromError(error);
             });
-        }
+    }  // google login
+
+    // register with email
+    const registerUser = (email,password,name) =>{
+            createUserWithEmailAndPassword(auth, email, password)
+                .then((result) => {
+                    const user = result.user;
+                    setAuthError('');
+                    const newUser = { email, displayName: name };
+                    setUser(newUser);
+                    // update profile: set user name
+                    updateProfile(auth.currentUser, {
+                        displayName: name
+                        }).then(() => {
+                       
+                        }).catch((error) => {
+                        
+                        });
+                        // end
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setAuthError(errorMessage);
+                });
+    } // registerUser
+
+    // login with email
+    const loginUser = (email, password) =>{
+            signInWithEmailAndPassword(auth, email, password)
+                .then((result) => {
+                    // Signed in 
+                    const user = result.user;
+                    setAuthError('');
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    setAuthError(errorMessage);
+                });
+    }
 
     // observer if user signin or not
     useEffect(()=>{
@@ -41,7 +82,7 @@ const useFirebase = () => {
                 }
         });
         return () => unsubscribed;
-    }, [auth])
+    }, [auth])  // end observer
 
     // logout user 
     const logoutUser = () => {
@@ -50,13 +91,15 @@ const useFirebase = () => {
         }).catch((error) => {
             // An error happened.
         })
-    }
+    } // end logout user
 
     return (
         {
             user,
+            authError,
             signInWithGoogle,
-            logoutUser
+            logoutUser,
+            registerUser
         }
     );
 };
