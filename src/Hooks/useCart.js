7c4d0f09cdec459843e405d7d83f8d5add2279dd
Sawom <asawom250@gmail.com>
2023-09-12 16:51:36 +0600

    admin

--- src/Hooks/useCart.js
+++ src/Hooks/useCart.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import useAuth from '../Pages/Authentication/useAuth/useAuth';
+import useAxiosSecure from './useAxiosSecure';
 
 // database theke cart er data fetch kore anar jnno use cart use korchi.
 //  tanstack query add kora
 const useCart = () => {
     const {user} = useAuth();
     const token = localStorage.getItem('access-token');
+    const [axiosSecure] = useAxiosSecure();
 
     const { refetch, data: cart = [] } = useQuery({
     queryKey: ['carts', user?.email],
+    // queryFn: async() =>{
+    //     const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, 
+    //     {
+    //         headers: {authorization: `bearer ${token}`}
+    //     })
+    //     return res.json();
+    //  },
     queryFn: async() =>{
-        const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, 
-        {
-            headers: {authorization: `bearer ${token}`}
-        })
-        return res.json();
-    }
+        const res = await axiosSecure(`/carts?email=${user?.email}`)
+        console.log('res from axios', res)
+        return res.data;
+     },
   })
 
 //   refetch ta return kore dite hobe.
