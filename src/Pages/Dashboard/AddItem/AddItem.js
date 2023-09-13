57348c6c5145ea81972ba908766963008bf73fc0
Sawom <asawom250@gmail.com>
2023-09-14 02:37:37 +0600

    hook form setup

--- src/Pages/Dashboard/AddItem/AddItem.js
+++ src/Pages/Dashboard/AddItem/AddItem.js
@@ -1,17 +1,28 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddItem = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    
+    const onSubmit = data => {
+        console.log(data);
+    }
+    console.log(errors);
+
     return (
         <div className='container mx-auto'>
-            <h1 className='text-3xl text-center'>add an item</h1>
-            {/* form */}
-            <form className='px-5' >
+            <section className='text-center mb-10 ' > 
+                <p className='text-blue-600 font-bold' >---What's new?---</p> <br />
+                <p className='text-xl   ' > <span className=' py-4 border-y-4 uppercase' > ADD AN ITEM </span> </p>
+            </section>
+            {/* form. react-hook-form diye kora eta */}
+            <form className='px-5' onSubmit={handleSubmit(onSubmit)} >
                 {/*1. recipe name */}
                 <div className="form-control w-full mb-6">
                     <label className="label">
                         <span className="label-text font-semibold">Recipe name*</span>
                     </label>
-                    <input type="text" placeholder="Recipe name" className="input input-bordered w-full " />
+                    <input type="text" placeholder="Recipe name" {...register("Recipe name", { required: true })}  className="input input-bordered w-full " required  />
                 </div>
                 {/* 2 div Category & price*/}
                 <div className='flex my-4'>
@@ -21,7 +32,7 @@ const AddItem = () => {
                             <span className="label-text font-semibold">Category*</span>
                         </label>
                         {/* select component daisy ui */}
-                        <select defaultValue="Pick One"  className="select select-bordered">
+                        <select defaultValue="Pick One"  {...register("category", { required: true })} className="select select-bordered">
                             <option disabled>Pick One</option>
                             <option>Pizza</option>
                             <option>Soup</option>
@@ -36,7 +47,7 @@ const AddItem = () => {
                         <label className="label">
                             <span className="label-text font-semibold">Price*</span>
                         </label>
-                        <input type="number" placeholder="Price" className="input input-bordered w-full " />
+                        <input type="number" placeholder="Price" {...register("Price", { required: true })} className="input input-bordered w-full " required  />
                     </div>
                 </div>
                 {/* 3 Recipe Details*/}
@@ -44,14 +55,14 @@ const AddItem = () => {
                     <label className="label">
                         <span className="label-text font-semibold">Recipe Details*</span>
                     </label>
-                    <textarea className="textarea textarea-bordered h-24" placeholder="Recipe Details"></textarea>
+                    <textarea className="textarea textarea-bordered h-24" placeholder="Recipe Details" {...register("Recipe Details", { required: true })} required ></textarea>
                 </div>
                 {/* 4 image */}
                 <div className="form-control w-full my-4">
                     <label className="label">
                         <span className="label-text">Item Image*</span>
                     </label>
-                    <input type="file" className="file-input file-input-bordered file-input-ghost w-full " />
+                    <input type="file" {...register("image", { required: true })} className="file-input file-input-bordered file-input-ghost w-full " required />
                 </div>
                 {/* submit button */}
                 <button className="btn btn-outline  border-4 mt-6">Add Item</button>
