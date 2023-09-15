1e50cacd3d2c16389696d47f016d67cb9648017c
Sawom <asawom250@gmail.com>
2023-09-16 00:51:47 +0600

    checkout form

---
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -0,0 +1,57 @@
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React from 'react';
+
+const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!stripe || !elements) {
+        // Stripe.js has not loaded yet. Make sure to disable
+        // form submission until Stripe.js has loaded.
+        return;
+        }
+
+        // Get a reference to a mounted CardElement. Elements knows how
+        // to find your CardElement because there can only ever be one of
+        // each type of element.
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+        return;
+        }
+
+    }
+
+
+    return (
+        <div>
+           {/* checkout form nicher link theke */}
+            {/* https://github.com/stripe/react-stripe-js/blob/master/examples/hooks/0-Card-Minimal.js */}
+             <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                    style: {
+                        base: {
+                        fontSize: '16px',
+                        color: '#424770',
+                        '::placeholder': {
+                            color: '#aab7c4',
+                        },
+                        },
+                        invalid: {
+                        color: '#9e2146',
+                        },
+                    },
+                    }}
+                />
+                <button type="submit" disabled={!stripe}>
+                    Pay
+                </button>
+            </form> 
+        </div>
+    );
+};
+
+export default CheckoutForm;
--- src/Pages/Dashboard/Payment/Payment.js
+++ src/Pages/Dashboard/Payment/Payment.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Payment = () => {
     return (
         <div>
-            <h1>payment</h1>
+            
         </div>
     );
 };
