38f65844a3d346d7e2b5f067152a49c30e3d6e18
Sawom <asawom250@gmail.com>
2023-09-18 16:31:27 +0600

    updated

--- src/Pages/Dashboard/AdminHome/AdminHome.js
+++ src/Pages/Dashboard/AdminHome/AdminHome.js
@@ -6,7 +6,7 @@ import { useQuery } from '@tanstack/react-query';
 const AdminHome = () => {
     const {user} = useAuth();
     const [axiosSecure] = useAxiosSecure();
-    
+
     // useAxiosSecure diye backend theke data load kore niye ashtechi
     const {data: stats = {} } = useQuery({
         queryKey : ['admin-stats'],
@@ -17,8 +17,32 @@ const AdminHome = () => {
     })
 
     return (
-        <div className='container mx-2'>
-            <h1 className='text-2xl '> Welcome <span className='text-blue-600'> {user.displayName}</span>  to Admin Home!</h1>
+        <div className='container '>
+            <h1 className='text-2xl mx-5'> Welcome <span className='text-blue-600'> {user.displayName}</span>  to Admin Home!</h1>
+            {/* stats */}
+            <div className="stats shadow">
+                {/* revenue */}
+                <div className="stat place-items-center">
+                    <div className="stat-title">Revenue</div>
+                    <div className="stat-value"> ${stats.revenue} </div>
+                </div>
+                {/* users */}
+                <div className="stat place-items-center">
+                    <div className="stat-title">Users</div>
+                    <div className="stat-value text-secondary"> {stats.users} person</div>
+                </div>
+                {/* menu items */}
+                <div className="stat place-items-center">
+                    <div className="stat-title">Menu Items</div>
+                    <div className="stat-value"> {stats.products} </div>
+                </div>
+                {/* orders */}
+                <div className="stat place-items-center">
+                    <div className="stat-title">Orders</div>
+                    <div className="stat-value"> {stats.orders} </div>
+                </div>
+            
+            </div>
         </div>
     );
 };
