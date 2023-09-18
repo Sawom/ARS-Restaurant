ff6dde481c98163a4ff287d006f5be69d340757e
Sawom <asawom250@gmail.com>
2023-09-18 15:48:44 +0600

    update

--- src/Pages/Dashboard/AdminHome/AdminHome.js
+++ src/Pages/Dashboard/AdminHome/AdminHome.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import useAuth from '../../Authentication/useAuth/useAuth';
+import useAxiosSecure from '../../../Hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const AdminHome = () => {
     const {user} = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    
+    // useAxiosSecure diye backend theke data load kore niye ashtechi
+    const {data: stats = {} } = useQuery({
+        queryKey : ['admin-stats'],
+        queryFn: async() =>{
+            const res = await axiosSecure('/admin-stats');
+            return res.data;
+        }
+    })
 
     return (
         <div className='container mx-2'>
